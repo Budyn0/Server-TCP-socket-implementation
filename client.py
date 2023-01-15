0440bb4eb392c7879e164141ed97619815693d05
Budyn0 <99538359+Budyn0@users.noreply.github.com>
2023-01-15 15:45:26 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,43 +1,48 @@
 import socket
 
-# Tworzenie gniazda
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def client():
+    # Tworzenie gniazda
+    client_socket = socket.socket(socket.AF_INET, socket.SOCKSTREAM)
 
-# Łączenie z serwerem
-server_address = ('127.0.0.1', 2525)
-print('Connecting to {}:{}'.format(*server_address))
-client_socket.connect(server_address)
+    # Podanie adresu IP serwera
+    server = input("Enter IP of the server: ")
+    port = 2525
 
-# Logowanie
-username = input('Enter your username: ')
-client_socket.sendall(username.encode())
+    # Łączenie z serwerem
+    client_socket.connect((server, port))
 
-# Otrzymanie powiadomienia o pomyślnym zalogowaniu
-response = client_socket.recv(1024).decode()
-print(response)
+    # Logowanie
+    username = input("Enter username: ")
+    client_socket.sendall(username.encode())
+    response = client_socket.recv(1024).decode()
+    print(response)
 
-while True:
-    # Wyświetlenie menu
-    print('1. Send email')
-    print('2. Read email')
-    print('3. Exit')
-    choice = input('Enter your choice: ')
+    while True:
+        # Menu
+        print("1. Send email")
+        print("2. Receive email")
+        print("3. Exit")
+        choice = input("Enter your choice: ")
 
-    # Wysłanie wiadomości e-mail
-    if choice == '1':
-        to = input('To: ')
-        message = input('Message: ')
-        email_format = 'From: {}\nTo: {}\n{}'
-        email = email_format.format(username, to, message)
-        client_socket.sendall(email.encode())
-        print('Email sent.')
-    # Odczytanie wiadomości e-mail
-    elif choice == '2':
-        client_socket.sendall(b'READ')
-        email = client_socket.recv(1024).decode()
-        print(email)
-    # Wyjście
-    elif choice == '3':
-        client_socket.sendall(b'EXIT')
-        print('Exiting...')
-        break
+        # Wysyłanie maila
+        if choice == '1':
+            to = input("To: ")
+            message = input("Message: ")
+            email = "From: {}\nTo: {}\n{}".format(username, to, message)
+            client_socket.sendall(email.encode())
+        # Odbieranie maila
+        elif choice == '2':
+            data = client_socket.recv(1024)
+            print("Received email: ")
+            print(data.decode())
+        # Wyjście z programu
+        elif choice == '3':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Try again.")
+    # Zakończenie połączenia
+    client_socket.close()
+
+if name == 'main':
+    client()
